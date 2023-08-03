0a0f8bd03fc9e340a1f47e29747df140fa092730
henryk <henryk.daneluz@wedigital.it>
2023-08-03 17:04:15 +0200

    minor fixes

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const path = require('path')
-const dotenv = require('dotenv')
-const librarySchemas = require('./lib/index')
-const MySqlDriver =  require('@mikro-orm/mysql').MySqlDriver
+const path = require('path');
+const dotenv = require('dotenv');
+const librarySchemas = require('./lib/index').default;
+const MySqlDriver =  require('@mikro-orm/mysql').MySqlDriver;
 
-let envFile = './.env.testing'
+let envFile = './.env.testing';
 if (!!process.env.NODE_ENV) {
-  envFile = `./../.env.${process.env.NODE_ENV}`
+  envFile = `./../.env.${process.env.NODE_ENV}`;
 }
-envFile = path.join(__dirname, envFile)
-// console.log(`Reading file: ${envFile}`)
-const env = dotenv.config({ path: envFile }).parsed
+envFile = path.join(__dirname, envFile);
+// console.log(`Reading file: ${envFile}`);
+const env = dotenv.config({ path: envFile }).parsed;
 if (!env) {
-  throw new Error(`A configuration file must be specified (.env), found nothing in: ${envFile}`)
+  throw new Error(`A configuration file must be specified (.env), found nothing in: ${envFile}`);
 }
-// console.log(`<d> Connecting with ${env.MYSQL_HOST}:${env.MYSQL_PORT} using ${env.MYSQL_USER} to database: ${env.MYSQL_DB_NAME}`)
+// console.log(`<d> Connecting with ${env.MYSQL_HOST}:${env.MYSQL_PORT} using ${env.MYSQL_USER} to database: ${env.MYSQL_DB_NAME}`);
 
 exports.default = {
   type: 'mysql',
@@ -24,4 +24,4 @@ exports.default = {
   schemaGenerator: {
     disableForeignKeys: true,
   }
-}
+};
--- mikro-orm.config.js
+++ mikro-orm.config.js
@@ -1,19 +1,20 @@
-const path = require('path')
-const dotenv = require('dotenv')
-const librarySchemas = require('./lib/index').default
-const MySqlDriver =  require('@mikro-orm/mysql').MySqlDriver
+const path = require('path');
+const dotenv = require('dotenv');
+const librarySchemas = require('./lib/index').default;
+const MySqlDriver =  require('@mikro-orm/mysql').MySqlDriver;
 
-let envFile = './.env.testing'
+let envFile = './.env.testing';
 if (!!process.env.NODE_ENV) {
-  envFile = `./../.env.${process.env.NODE_ENV}`
+  envFile = `./../.env.${process.env.NODE_ENV}`;
 }
-envFile = path.join(__dirname, envFile)
+envFile = path.join(__dirname, envFile);
 // console.log(`Reading file: ${envFile}`)
-const env = dotenv.config({ path: envFile }).parsed
+const env = dotenv.config({ path: envFile }).parsed;
 if (!env) {
-  throw new Error(`A configuration file must be specified (.env), found nothing in: ${envFile}`)
+  throw new Error(`A configuration file must be specified (.env), found nothing in: ${envFile}`);
 }
-console.log(`<d> Connecting with ${env.MYSQL_HOST}:${env.MYSQL_PORT} using ${env.MYSQL_USER} to database: ${env.MYSQL_DB_NAME}`)
+console.log(`<d> Connecting with ${env.MYSQL_HOST}:${env.MYSQL_PORT} using ${env.MYSQL_USER} to database: ${env.MYSQL_DB_NAME}`);
+console.log(librarySchemas[0]._meta.properties);
 
 exports.default = {
   type: 'mysql',
@@ -21,7 +22,8 @@ exports.default = {
   dbName: env.MYSQL_DB_NAME || 'test_users',
   password: env.MYSQL_PASS || 'mysqlroot',
   entities: librarySchemas,
+  useTsNode: false,
   schemaGenerator: {
     disableForeignKeys: true,
   }
-}
+};
